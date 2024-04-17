714c44259cf7ad4803dacede8498d5ad33be6e01
Serega <154548637+SeregaLemaev2004@users.noreply.github.com>
2024-04-17 07:15:04 +0300

    first commit

---
+++ app/src/main/java/ru/btpit/zadanie2/activity/NewPostFragment.kt
@@ -0,0 +1,88 @@
+package ru.btpit.zadanie2.activity
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+
+import ru.btpit.zadanie2.databinding.FragmentNewPostBinding
+import ru.btpit.zadanie2.util.StringArg
+
+import ru.netology.nmedia.util.AndroidUtils
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class NewPostFragment : Fragment() {
+
+    companion object {
+        var Bundle.textArg: String? by StringArg
+    }
+
+    private val viewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentNewPostBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        if (arguments?.textArg == null )
+        {
+            binding.Name.text ="Добавить пост"
+        }
+     else
+        {
+            binding.Name.text ="Изменение поста"
+        }
+
+        arguments?.textArg
+            ?.let(binding.edit::setText)
+
+        binding.ok.setOnClickListener {
+            viewModel.changeContent(binding.edit.text.toString())
+            viewModel.save()
+            AndroidUtils.hideKeyboard(requireView())
+            findNavController().navigateUp()
+        }
+        return binding.root
+    }
+}
+////    override fun onCreate(savedInstanceState: Bundle?) {
+////        super.onCreate(savedInstanceState)
+////        val binding = ActivityNewPostBinding.inflate(layoutInflater)
+////        setContentView(binding.root)
+//
+//        val text = intent.getStringExtra(Intent.EXTRA_TEXT)
+//        binding.edit.text = Editable.Factory.getInstance().newEditable(text)
+//        if (text == "")
+//        {
+//            binding.Name.text = "Создание поста"
+//        }
+//        else binding.Name.text = "Редактирование поста"
+//        binding.edit.requestFocus()
+//        binding.ok.setOnClickListener {
+//            val intent = Intent()
+//            if (binding.edit.text.isNullOrBlank()) {
+//                setResult(Activity.RESULT_CANCELED, intent)
+//            } else {
+//                val content = binding.edit.text.toString()
+//                intent.putExtra(Intent.EXTRA_TEXT, content)
+//                setResult(Activity.RESULT_OK, intent)
+//            }
+//            finish()
+//        }
+//
+//    }
+//}
---
+++ app/src/main/java/ru/btpit/zadanie2/viewmodel/PostViewModel.kt
@@ -0,0 +1,59 @@
+package ru.netology.nmedia.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryFileImpl
+import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositorySharedPrefsImpl
+
+private  val  empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    isLiked = false,
+    published = "",
+    likecount = 0,
+    share = 0
+
+
+)
+class PostViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: PostRepository = PostRepositoryFileImpl(application)
+    val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+    val selectedPost = MutableLiveData<Post>()
+    fun save()
+    {
+        edited.value?.let {
+            repository.save(it)
+        }
+        edited.value = empty
+    }
+    fun edit(post: Post)
+    {
+        edited.value = post
+    }
+    fun  changeContent(content: String)
+    {
+        edited.value?.let {
+            val text = content.trim()
+            if (it.content == text){
+                return
+            }
+            edited.value = it.copy(content = text)
+        }
+    }
+    fun like(id: Long) = repository.like(id)
+    fun share(id: Long) = repository.share(id)
+    fun removeById(id: Long) = repository.removeById(id)
+    fun  getPostById(id: Long){
+        repository.postID(id).observeForever{
+            selectedPost.value = it
+        }
+    }
+}
